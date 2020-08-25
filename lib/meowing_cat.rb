c156e4dd9c66fa64f432ef491c5730e636017bba
Newland-A <ajn252@gmail.com>
2020-08-25 09:04:52 -0400

    Done.

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,12 @@
 ## code your solution here. 
+class Cat
+  #creates the getter method for you.
+  #a shortcut
+  attr_accessor :name
+#defines a method for the cat to be able to meow
+  def meow
+    #puts meow when the cat meows
+    puts "meow!"
+  end
+
+end
